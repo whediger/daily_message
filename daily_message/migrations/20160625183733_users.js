24751ca531bfccb3a8bc3e18ac8487ca8d56e241
Wes Hediger <wes.hediger@gmail.com>
2016-06-25 18:43:01 -0600

    create migration file for table users

---
+++ daily_message/migrations/20160625183733_users.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('users', function(table){
+      table.increments();
+      table.string('username');
+      table.string('password');
+      table.string('first_name');
+      table.string('last_name');
+      table.string('email');
+      table.integer('phone_number');
+    });
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.droptable('users');
+};
